946c50b43f24e461329c3226ec8b2b4aa67e0dc0
HasinJishan <hasinjishan17@gmail.com>
2026-09-03 16:35:29 +0530

    Point frontend to live Render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend URL
+  baseURL: 'https://stockflow-wms-backend.onrender.com', // Your backend URL
 });
 
 export default instance;
