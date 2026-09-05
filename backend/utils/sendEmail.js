b6b4c7fa666e8ee86789551b37829495713014f8
HasinJishan <hasinjishan17@gmail.com>
2026-09-05 12:36:35 +0530

    Force IPv4 in nodemailer to fix Render ENETUNREACH error

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,18 +2,18 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, // Use SSL
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-        tls: {
-            rejectUnauthorized: false
-        },
-        family: 4 // Force IPv4 - fixes Render's ENETUNREACH error over IPv6
-    });
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // STARTTLS instead of SSL
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false
+    },
+    family: 4
+});
 
     const mailOptions = {
         from: `"StockFlow WMS" <${process.env.EMAIL_USER}>`,
